fd433ed2b38ca7725bb6cd55c21d0072f4685269
abdul <abdul.tanveer@gmail.com>
2024-07-30 10:21:09 +0530

    conditional rendering a list items

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import Greet from "./components/Greet";
 import Message from "./components/Message";
+import PackingList from "./components/PackingList";
 import UserGreeting from "./components/UserGreeting ";
 
 function App() {
   return (
     <div className="App">
+
+    <PackingList/>
     <UserGreeting/>
     </div>
   );
