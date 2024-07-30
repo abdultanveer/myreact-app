5e46f61ae651ca296ca10db991be4ca3aae1d295
abdul <abdul.tanveer@gmail.com>
2024-07-30 12:44:13 +0530

    styles as modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Inline from "./styles/Inline";
-import StyleSheet from "./styles/StyleSheet";
+import StyleSheet from './styles/StyleSheet';
+
+import styles from './styles/applyStyles.css';
+import  './styles/applyStyles.module.css';
+
 
 
 function App() {
   return (
     <div className="App">
+    <h1 className="error">ERROR</h1>
+    <h1 className="success">Success</h1>
      <StyleSheet  primary = {true}/>
      <Inline/>
     </div>
