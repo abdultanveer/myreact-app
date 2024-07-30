8e17818340bd32afc75ce39ed4750b93a5681942
abdul <abdul.tanveer@gmail.com>
2024-07-30 14:53:29 +0530

    handle change,submission

--- src/formcomponents/Form.js
+++ src/formcomponents/Form.js
@@ -1,33 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Form extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         userName: ''
-      }
+  constructor(props) {
+    super(props);
 
-     this.handleUserNameChange = this.handleUserNameChange.bind(this)
-    }
+    this.state = {
+      userName: "",
+      comments: "",
+      topic:"Angular"
+    };
+
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handleCommentsChange = this.handleCommentsChange.bind(this);
+    this.handleTopicChange = this.handleTopicChange.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+
+  }
+
+  handleTopicChange(event) {
+    this.setState({ topic: event.target.value });
+  }
+
+  handleUserNameChange(event) {
+    this.setState({ userName: event.target.value });
+  }
+
+  handleCommentsChange(event) {
+    this.setState({ comments: event.target.value });
+  }
 
 
-    handleUserNameChange = (event) => {
-        this.setState = ({
-            userName: event.target.value
-        })
-    }
+  handleSubmission(event){
+    alert(`${this.state.userName}${this.state.comments} ${this.state.topic}`)
+    event.preventDefault() //dont want the values to disappear
+  }
 
   render() {
     return (
-        <form>
-      <div>
-        <label>User name</label>
-        <input type='text' value={this.state.userName}  onChange={this.handleUserNameChange}/>
-      </div>
+      <form   onSubmit={this.handleSubmission}>
+        <div>
+          <label>User name</label>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={this.handleUserNameChange}
+          />
+        </div>
+        <div>
+          <label>comments</label>
+          <textarea
+            value={this.state.comments}
+            onChange={this.handleCommentsChange}
+          />
+        </div>
+        <div>
+            <select  value={this.state.topic} onChange={this.handleTopicChange}>
+                <option> React</option>
+                <option> Angular</option>
+                <option> Android</option>
+
+            </select>
+        </div>
+        <button type="submit">Submit</button>
       </form>
-    )
+    );
   }
 }
 
-export default Form
+export default Form;
