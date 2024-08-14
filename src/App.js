75c117a007dfcb1935430aa222782f7c561a5267
abdul <abdul.tanveer@gmail.com>
2024-08-14 15:09:16 +0530

    datafetching with reducer and managing the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { createContext, useReducer } from "react";
 import "./App.css";
 import DataFetchingOne from "./hooks/useReducer/DataFetchingOne";
+import DataFetchingTwo from "./hooks/useReducer/DataFetchingTwo";
 
 function App() {
   return (
     <div>
-      <DataFetchingOne />
+      <DataFetchingTwo />
     </div>
   );
 }
